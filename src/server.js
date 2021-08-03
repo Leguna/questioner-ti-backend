aa2980d1d927d5fcde91f61cdd10ab674f451bda
Leguna <ahmadtuflihunxd@gmail.com>
2021-08-03 16:31:15 +0700

    hem

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const { validate } = require('./database')
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
+    port: 8080,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: true,
